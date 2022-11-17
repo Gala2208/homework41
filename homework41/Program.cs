5ac58c7ee36919b7485ee197af77381930b1c397
Gala2208 <toppers.ogre-0k@icloud.com>
2022-11-17 15:06:09 +0500

    Create

---
+++ homework41/Program.cs
@@ -0,0 +1,12 @@
+﻿int m = Convert.ToInt32(Console.ReadLine()),
+    number, count = 0;
+
+Console.WriteLine(m);
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("Число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) count++;
+}
+
+Console.WriteLine("Чисел больше 0: " + count);
